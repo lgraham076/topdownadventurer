34625af16403fd9bac83cd48658e27253d12e0c1
lgraham076 <langstongraham@gmail.com>
2025-02-24 22:45:03 -0600

    Fixed misaligned animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,35 @@
 extends CharacterBody2D
 
+const DEFAULT_DIRECTION := "down"
+const DEFAULT_ACTION := "idle"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var direction := DEFAULT_DIRECTION
+var action := DEFAULT_ACTION
 
+func get_directedaction()->String:
+	return action + "_" + direction
+	
+func animate()->void:
+	$AnimatedSprite2D.animation = get_directedaction()
+	$AnimatedSprite2D.play()
+	await $AnimatedSprite2D.animation_finished
+	action = DEFAULT_ACTION
+	return
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	
 	if Input.is_action_just_pressed("move_down"):
-		$AnimatedSprite2D.animation = "idle_down"
+		direction = "down"
 	elif Input.is_action_just_pressed("move_up"):
-		$AnimatedSprite2D.animation = "idle_up"
+		direction = "up"
 	elif Input.is_action_just_pressed("move_left"):
-		$AnimatedSprite2D.animation = "idle_left"
+		direction = "left"
 	elif Input.is_action_just_pressed("move_right"):
-		$AnimatedSprite2D.animation = "idle_right"
-
+		direction = "right"
+		
+	if Input.is_action_just_pressed("attack"):
+		action = "attack"
+		
+	animate()
+	
 	move_and_slide()
