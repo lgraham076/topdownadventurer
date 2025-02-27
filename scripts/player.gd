05ec73351e01654f801008a699e024a53314e231
lgraham076 <langstongraham@gmail.com>
2025-02-26 21:31:52 -0600

    Added player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,8 @@ const DEFAULT_ACTION := "idle"
 var direction := DEFAULT_DIRECTION
 var action := DEFAULT_ACTION
 
+const SPEED := 75.0
+
 func get_directedaction()->String:
 	return action + "_" + direction
 	
@@ -16,21 +18,30 @@ func animate()->void:
 	action = DEFAULT_ACTION
 	return
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	
 	if Input.is_action_just_pressed("move_down"):
 		action = "run"
 		direction = "down"
+		velocity.y += 1
 	elif Input.is_action_just_pressed("move_up"):
 		action = "run"
 		direction = "up"
+		velocity.y -= 1
 	elif Input.is_action_just_pressed("move_left"):
 		action = "run"
 		direction = "left"
+		velocity.x -= 1
 	elif Input.is_action_just_pressed("move_right"):
 		action = "run"
 		direction = "right"
+		velocity.x += 1
 	elif Input.is_action_just_pressed("attack"):
 		action = "attack"
+		
+	velocity = velocity.normalized() * SPEED
+	position += velocity * delta
 		
 	animate()
 	
